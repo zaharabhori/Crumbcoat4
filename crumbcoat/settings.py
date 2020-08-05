75e54ec288a03701d8641de3fe67964d742c9a20
ZaharaBhori <zaharabhori@gmail.com>
2020-08-06 00:12:42 +0530

    hello

--- crumbcoat/settings.py
+++ crumbcoat/settings.py
@@ -141,3 +141,7 @@ AWS_STORAGE_BUCKET_NAME = 'crumbcoat-bucket'
 AWS_S3_FILE_OVERWRITE = False
 AWS_DEFAULT_ACL = None
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+AWS_S3_HOST = 's3.ap.south-1.amazonaws.com'
+AWS_S3_REGION_NAME = 'ap-south-1'
+
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
